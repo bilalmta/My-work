db809f18bda13cc9e9c71c272faedb65a09fe8a8
essamhassan <es.hassan187@gmail.com>
2013-12-15 15:26:14 +0200

    Create Main.cpp
    Main File

---
+++ OOP-Project-1-P1/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Matrix.h"
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
